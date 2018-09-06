5a3585dbed8f41cebd64c0f07dceeaa539e908e1
danielfrancoeur <daniel.francoeur65@gmail.com>
2018-09-06 18:36:38 -0400

    Deals have active investors through active investments:

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,4 +2,12 @@ class Deal < ApplicationRecord
   has_many :investments
   has_one :loan
   belongs_to :user, foreign_key: 'borrower_id'
+  has_many :active_investments, -> { where status: 'invested' },
+           class_name: 'Investment',
+           foreign_key: 'deal_id'
+  has_many :received_investments, -> { where status: 'received' },
+           class_name: 'Investment',
+           foreign_key: 'deal_id'
+  has_many :investors, through: :investments, source: :user
+  has_many :active_investors, through: :active_investments, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   has_many :investments
-  has_many :invested_deals, through: :investments, source: :deal
+  has_many :active_investments, -> { where status: 'invested' },
+           class_name: 'Investment',
+           foreign_key: 'user_id'
+  has_many :received_investments, -> { where status: 'received' },
+           class_name: 'Investment',
+           foreign_key: 'user_id'
+  has_many :active_deals, through: :investments, source: :deal
 end
