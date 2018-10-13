b8d6372e51d8d2d72f514cc79b7714d045357b0b
danielfrancoeur <daniel.francoeur65@gmail.com>
2018-10-13 17:00:17 -0400

    fixed bug in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,7 +52,7 @@ end
     deal_id: index + 1,
     contract_amount: Faker::Number.between(100, 500) * 1000,
     origination_date: Faker::Date.between(70.days.ago, Date.today),
-    annual_percentage_rate: Faker::Number.between(80, 130) / 10.round(2),
+    annual_percentage_rate: Faker::Number.between(80, 130) / 1000.0,
     origination_fee_percentage_rate: Faker::Number.between(20, 40) / 10.round(2)
   )
   loan.rehab_budget_amount = Faker::Number.between(1, 5) * 0.1 * loan.contract_amount
